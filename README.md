982dcc354968d52ba45f015c871a86459ac0b9c6
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-05 01:24:26 +0100

    add calculator complete

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# This is an hour hackathon project
+# This is an hour hackathon project (Custom Calculator API )
 
 ## Overview
 
-It is a project that
+It is a project that develops a FastAPI application that acts as a custom calculator. This API should perform various arithmetic operations, conversions, and even some basic logical operations. The focus of this project is to implement logic in endpoint functions, handle different types of parameters, and return calculated results based on the input provided.
+
+## Endpoint Details
+
+### GET endpoint
+
+### POST endpoint
+
+### PUT endpoint
+
+### DELETE endpoint
--- main.py
+++ main.py
@@ -1,6 +1,55 @@
 from fastapi import FastAPI
-from routes import router
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.router = router
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+    operation: str
+
+
+# class SubtractNumber(BaseModel):
+#     num1: float
+#     num2: float
+#     operation: str
+
+
+# class MultiplyNumber(BaseModel):
+#     num1: float
+#     num2: float
+#     operation: str
+
+
+# class DivideNumber(BaseModel):
+#     num1: float
+#     num2: float
+#     operation: str
+
+
+@app.post("/calculate")
+async def addition(numbers: Numbers):
+    num1 = numbers.num1
+    num2 = numbers.num2
+    operator = numbers.operation
+
+    if num1 and num2 and operator:
+        if operator == '+':
+            return num1 + num2
+
+        if operator == '-':
+            return num1 - num2
+
+        if operator == '*':
+            return num1 * num2
+
+        if operator == '/':
+            return num1 / num2
+
+    return {"message": "Add a valid number and opperator"}
+
+
+@app.post("/convert/temperature")
+async def conversion(string):
+    return {"message": "Convert temperature"}
