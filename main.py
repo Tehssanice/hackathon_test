72f854979dc4112f2cfabe6328c783440c350189
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-05 15:41:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,32 +8,14 @@ app = FastAPI()
 class Numbers(BaseModel):
     num1: float
     num2: float
-    operation: str
-
-
-# class SubtractNumber(BaseModel):
-#     num1: float
-#     num2: float
-#     operation: str
-
-
-# class MultiplyNumber(BaseModel):
-#     num1: float
-#     num2: float
-#     operation: str
-
-
-# class DivideNumber(BaseModel):
-#     num1: float
-#     num2: float
-#     operation: str
+    operator: str
 
 
 @app.post("/calculate")
-async def addition(numbers: Numbers):
+async def operations(numbers: Numbers):
     num1 = numbers.num1
     num2 = numbers.num2
-    operator = numbers.operation
+    operator = numbers.operator
 
     if num1 and num2 and operator:
         if operator == '+':
@@ -45,8 +27,10 @@ async def addition(numbers: Numbers):
         if operator == '*':
             return num1 * num2
 
-        if operator == '/':
+        if operator == '/' and (num1 or num2 != 0):
             return num1 / num2
+        else:
+            return {"message": "Can not devide zero"}
 
     return {"message": "Add a valid number and opperator"}
 
@@ -61,21 +45,3 @@ async def conversion(temp=Annotated[str | None, Query()], choices=["Celsius", "F
 
     if temp in choices == "Fahrenheit":
         return {"Fahrenheit": fahrenheit}
-
-    # degree = int(temp[-1])
-
-    # i_convention = temp[-1]
-
-    # if i_convention.upper() == "C":
-    #     result = int(round((9 * degree) / 5 + 32))
-    #     o_convention = "Fahrenheit"
-    # elif i_convention.upper() == "F":
-    #     result = int(round((degree - 32) * 5 / 9))
-    #     o_convention = "Celsius"
-    # else:
-    #     print("Input proper convention.")
-    #     quit()
-
-    # return ("The temperature in", o_convention, "is", result, "degrees.")
-
-    # return {"message": "Convert temperature"}
