bd384b9fee6103400a9a3fd8a881b145eaa14d1b
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-05 18:47:46 +0100

    update remove player

--- adventure_game_api/main.py
+++ adventure_game_api/main.py
@@ -1,5 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
+from typing import Annotated
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+
+player_inventory = []
+
+game_location = {
+  
+  "Kaduna": {"description": "north", "items_available":"available"},
+  "Abuja": {"description": "central", "items_available":"available"},
+  "Enugu": {"description": "east", "items_available":"not-available"},
+  
+}
+
+class Player(BaseModel):
+    name: str
+    description: str
+    location: str
+
+@app.post("/inventory/add")
+async def add_to_inventory(player: Annotated[Player, Body(...)]):
+    try:
+        if player.name in [i['name'] for i in player_inventory]:
+            raise HTTPException(status_code=400, detail="Item already exists in inventory")
+          
+        player_inventory.append(player.dict())
+      
+        return JSONResponse(status_code=200, content= {"data": player_inventory, "message": "Item added to inventory"})
+    except Exception as e:
+        return {"message": str(e)}
+
+
+@app.post("/inventory/remove")
+async def remove_from_inventory(player: Annotated[Player, Body(...)]):
+  try:
+    if player.name not in [i['name'] for i in player_inventory]:
+      raise HTTPException(status_code=400, detail="Item does not exist in inventory")
+      
+    player_inventory.remove(player.dict())
+  
+    return JSONResponse(status_code=200, content= {"data": player_inventory, "message": "Item removed from inventory"})
+  except Exception as e:
+    return {"message": str(e)}
+
+
+  
