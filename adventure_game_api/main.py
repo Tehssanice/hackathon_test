0810051b3ee363d55a8f8be7213a88e77602b445
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-12 16:38:12 +0100

    new updates

--- adventure_game_api/main.py
+++ adventure_game_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body, HTTPException
+from fastapi import FastAPI, Body, HTTPException, Query
 from pydantic import BaseModel
 from typing import Annotated
 from fastapi.responses import JSONResponse
@@ -8,11 +8,11 @@ app = FastAPI()
 
 player_inventory = []
 
-game_location = {
+game_locations = {
   
   "Kaduna": {"description": "north", "items_available":"available"},
   "Abuja": {"description": "central", "items_available":"available"},
-  "Enugu": {"description": "east", "items_available":"not-available"},
+  "Enugu": {"description": "east", "items_available":"not available"},
   
 }
 
@@ -47,4 +47,18 @@ async def remove_from_inventory(player: Annotated[Player, Body(...)]):
     return {"message": str(e)}
 
 
+@app.get("/explore/{location_name}")
+async def explore_location(location_name:str):
+  
+  try:
+    for key, value in game_locations.items():
+      print(key, "test")
+      if key.lower() == location_name.lower():
+        return {"message": f" {location_name.title()} is {value['items_available']}", "location": location_name.title()}
+    else:
+      raise HTTPException(status_code=404, detail= "Location does not exist yet")
+    
+
+  except Exception as e:
+    return{"message": str(e)}
   
